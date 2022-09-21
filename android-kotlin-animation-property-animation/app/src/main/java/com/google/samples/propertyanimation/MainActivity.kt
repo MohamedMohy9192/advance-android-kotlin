850f2226bca27f4395cbaa6268cb4036de75b305
Mohamed Mohy <mohamed.mohy.info@gmail.com>
2022-09-21 22:34:41 +0200

    Add rotating animation to the Star

--- android-kotlin-animation-property-animation/app/src/main/java/com/google/samples/propertyanimation/MainActivity.kt
+++ android-kotlin-animation-property-animation/app/src/main/java/com/google/samples/propertyanimation/MainActivity.kt
@@ -16,8 +16,12 @@
 
 package com.google.samples.propertyanimation
 
+import android.animation.Animator
+import android.animation.AnimatorListenerAdapter
+import android.animation.ObjectAnimator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.ImageView
 
@@ -69,7 +73,44 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    /**
+     * create an animation that rotates the ImageView containing the star from a value of -360 to 0.
+     * This means that the view, and thus the star inside it, will rotate in a full circle (360 degrees) around its center.
+     */
     private fun rotater() {
+        // The reason that the animation starts at -360 is that that allows the star to complete a full circle (360 degrees)
+        // and end at 0, which is the default rotation value for a non-rotated view,
+        // so it's a good value to have at the end of the animation (in case any other action occurs on that view later, expecting the default value).
+        // Note that the start starts at 0 degrees, before the animation begins, and then jumps immediately to -360 degrees.
+        // But since -360 is visually the same as 0 degrees, there is no noticeable change when the animation begins.
+        val animator = ObjectAnimator.ofFloat(star, View.ROTATION, -360f, 0f)
+
+        // Change the duration property of the animator to 1000 milliseconds
+        animator.duration = 1000
+
+        // you ran the animation again before it came to a stop.
+        // Did you notice a jump when you clicked on the button?
+        // This is because you always reset to -360 degrees at the start of the animation,
+        // regardless of whether the star is currently in the middle of animating or not.
+
+        //  you're going to just prevent the user from clicking the button while the animation is running,
+        //  to allow them to fully enjoy the in-process animation first.
+
+        // Animators have a concept of listeners, which call back into user code to notify the application of changes in the state of the animation.
+        // you'd like to disable the ROTATE button as soon as the animation starts, and then re-enable it when the animation ends.
+
+        animator.addListener(object : AnimatorListenerAdapter() {
+            override fun onAnimationStart(animation: Animator) {
+                rotateButton.isEnabled = false
+            }
+            override fun onAnimationEnd(animation: Animator) {
+                rotateButton.isEnabled = true
+            }
+        })
+
+        // star spin around its center. But it does so really quickly.
+        // In fact, it does it in 300 milliseconds, which is the default duration of all animations on the platform.
+        animator.start()
     }
 
     private fun translater() {
