2e88b114785f099766bb34a479dbe1bb6592f3b3
Mohamed Mohy <mohamed.mohy.info@gmail.com>
2022-09-29 10:35:21 +0200

    Add markers

--- Wander/app/src/main/java/com/example/android/wander/MapsActivity.kt
+++ Wander/app/src/main/java/com/example/android/wander/MapsActivity.kt
@@ -12,6 +12,7 @@ import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 import com.example.android.wander.databinding.ActivityMapsBinding
+import java.util.*
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
@@ -42,10 +43,53 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
     override fun onMapReady(googleMap: GoogleMap) {
         map = googleMap
 
-        // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        map.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        map.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        val latitude = 31.193444
+        val longitude = 29.908779
+        val homeLatLng = LatLng(latitude, longitude)
+        // The zoom level controls how zoomed in you are on the map.
+        val zoomLevel = 15f
+        // Move the camera to homeLatLng
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(homeLatLng, zoomLevel))
+        // Add a marker to the map at homeLatLng.
+        map.addMarker(MarkerOptions().position(homeLatLng))
+        setMapLongClick(map)
+        setPoiClick(map)
+
+    }
+
+    private fun setMapLongClick(map: GoogleMap) {
+        map.setOnMapLongClickListener { latLng ->
+            // A snippet is additional text that's displayed after the title.
+            val snippet = String.format(
+                Locale.getDefault(),
+                "Lat: %1$.5f, Long: %2$.5f",
+                latLng.latitude,
+                latLng.longitude
+            )
+
+            // Set the title of the marker to Dropped Pin using a R.string.dropped_pin string resource.
+            map.addMarker(
+                MarkerOptions()
+                    .position(latLng)
+                    .title(getString(R.string.dropped_pin))
+                    .snippet(snippet)
+            )
+        }
+    }
+
+
+    private fun setPoiClick(map: GoogleMap) {
+        // This click listener places a marker on the map immediately when the user clicks a POI.
+        // The click listener also displays an info window that contains the POI name.
+        map.setOnPoiClickListener { poi ->
+            val poiMarker = map.addMarker(
+                MarkerOptions()
+                    .position(poi.latLng)
+                    .title(poi.name)
+            )
+            // Show the info window.
+            poiMarker?.showInfoWindow()
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
