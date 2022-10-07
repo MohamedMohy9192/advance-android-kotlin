586eaf5cc28f2825309b0fae38f077419ccfd119
Mohamed Mohy <mohamed.mohy.info@gmail.com>
2022-10-08 00:13:31 +0200

    Update task view model

---
+++ android-testing/app/src/test/java/com/example/android/architecture/blueprints/todoapp/MainCoroutineRule.kt
@@ -0,0 +1,37 @@
+package com.example.android.architecture.blueprints.todoapp
+
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.*
+import org.junit.rules.TestWatcher
+import org.junit.runner.Description
+
+
+/**
+ * Sets the main coroutines dispatcher to a [TestDispatcher] for unit testing.
+ *
+ * Declare it as a JUnit Rule:
+ *
+ * ```
+ * @get:Rule
+ * val mainCoroutineRule = MainCoroutineRule()
+ * ```
+ *
+ * Then, use `runTest` to execute your tests.
+ */
+@ExperimentalCoroutinesApi
+class MainCoroutineRule(
+    val testDispatcher: TestDispatcher = UnconfinedTestDispatcher()
+) : TestWatcher() {
+
+    override fun starting(description: Description?) {
+        super.starting(description)
+        Dispatchers.setMain(testDispatcher)
+    }
+
+    override fun finished(description: Description?) {
+        super.finished(description)
+        Dispatchers.resetMain()
+    }
+}
